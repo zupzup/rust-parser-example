2fad3566c2a77cb07f9ea3074e45d0d0321c554c
Mario Zupan <mario@zupzup.org>
2020-11-03 19:57:40 +0100

    progress

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,17 @@
 /// Basic HTTP Parser
 use nom::{
     branch::alt,
-    bytes::complete::{tag, tag_no_case, take_until, take_while},
-    character::complete::{alphanumeric0, alphanumeric1},
+    bytes::complete::{tag, tag_no_case, take, take_until, take_while},
+    character::{
+        complete::{alpha1, alphanumeric0, alphanumeric1, digit1, one_of},
+        is_alphanumeric,
+    },
     combinator::{cond, opt},
+    error::Error,
     error::ErrorKind,
-    multi::many1,
-    sequence::{pair, separated_pair, terminated},
-    Err as NomErr, IResult,
+    multi::{count, many0, many1, many_m_n, separated_list1},
+    sequence::{pair, separated_pair, terminated, tuple},
+    AsChar, Err as NomErr, IResult, InputTakeAtPosition,
 };
 
 #[derive(Debug, PartialEq, Eq)]
@@ -77,19 +81,64 @@ fn scheme(input: &str) -> IResult<&str, Scheme> {
         .and_then(|(next_input, res)| Ok((next_input, res.into())))
 }
 
-fn username(input: &str) -> IResult<&str, &str> {
-    alt((take_until(":"), take_until("@")))(input)
-}
-
-fn password(input: &str) -> IResult<&str, &str> {
-    take_until("@")(input)
-}
-
 fn authority(input: &str) -> IResult<&str, Option<(&str, Option<&str>)>> {
     opt(terminated(
         separated_pair(alphanumeric1, opt(tag(":")), opt(alphanumeric1)),
         tag("@"),
     ))(input)
+}
+
+// fn host_or_ip(input: &str) -> IResult<&str, String> {}
+
+fn host(input: &str) -> IResult<&str, String> {
+    alt((
+        tuple((many1(terminated(alphanumerichyphen1, tag("."))), alpha1)),
+        tuple((many_m_n(1, 1, alphanumerichyphen1), take(0 as usize))),
+    ))(input)
+    .and_then(|(next_input, mut res)| {
+        println!("res: {:?}", res);
+        if !res.1.is_empty() {
+            res.0.push(res.1);
+        }
+        Ok((next_input, res.0.join(".")))
+    })
+}
+
+fn alphanumerichyphen1<T>(i: T) -> IResult<T, T>
+where
+    T: InputTakeAtPosition,
+    <T as InputTakeAtPosition>::Item: AsChar,
+{
+    i.split_at_position1_complete(
+        |item| {
+            let char_item = item.as_char();
+            // println!(
+            //     "char: {} res: {}",
+            //     char_item,
+            //     !(char_item == '-') && !char_item.is_alphanum()
+            // );
+            !(char_item == '-') && !char_item.is_alphanum()
+        },
+        ErrorKind::AlphaNumeric,
+    )
+}
+
+// only IPv4
+fn ip(input: &str) -> IResult<&str, String> {
+    tuple((
+        count(
+            terminated(many_m_n(1, 3, one_of("0123456789")), tag(".")),
+            3,
+        ),
+        many_m_n(1, 3, one_of("0123456789")), // TODO: build one_digit parser
+    ))(input)
+    .and_then(|(next_input, res)| {
+        let mut first_three: Vec<String> = res.0.into_iter().flatten().collect::<Vec<String>>(); // TODO: FIXME
+        let last: String = res.1.into_iter().collect::<String>();
+        first_three.push(last);
+
+        Ok((next_input, first_three.join(".")))
+    })
 }
 
 fn request_method(input: &str) -> IResult<&str, Method> {
@@ -114,7 +163,7 @@ fn test_request_method() {
     assert_eq!(request_method("GET 1234"), Ok((" 1234", Method::GET)));
     assert_eq!(
         request_method("1234"),
-        Err(NomErr::Error(("1234", ErrorKind::Tag)))
+        Err(NomErr::Error(Error::new("1234", ErrorKind::Tag)))
     );
     assert_eq!(request_method("PUT POST"), Ok((" POST", Method::PUT)));
 }
@@ -136,4 +185,59 @@ fn test_authority() {
         Ok(("username:passwordzupzup.org", None))
     );
     assert_eq!(authority("@zupzup.org"), Ok(("@zupzup.org", None)));
+}
+
+#[test]
+fn test_host() {
+    assert_eq!(
+        host("localhost:8080"),
+        Ok((":8080", "localhost".to_string()))
+    );
+    assert_eq!(
+        host("example.org:8080"),
+        Ok((":8080", "example.org".to_string()))
+    );
+    assert_eq!(
+        host("some-subsite.example.org:8080"),
+        Ok((":8080", "some-subsite.example.org".to_string()))
+    );
+    assert_eq!(host("example.123"), Ok((".123", "example".to_string())));
+    assert_eq!(
+        host("$$$.com"),
+        Err(NomErr::Error(Error::new(
+            "$$$.com",
+            ErrorKind::AlphaNumeric
+        )))
+    );
+    assert_eq!(
+        host(".com"),
+        Err(NomErr::Error(Error::new(".com", ErrorKind::AlphaNumeric)))
+    );
+}
+
+#[test]
+fn test_ipv4() {
+    assert_eq!(
+        ip("192.168.0.1:8080"),
+        Ok((":8080", "192.168.0.1".to_string()))
+    );
+    assert_eq!(ip("0.0.0.0:8080"), Ok((":8080", "0.0.0.0".to_string())));
+    assert_eq!(
+        ip("1924.168.0.1:8080"),
+        Err(NomErr::Error(Error::new(
+            ".168.0.1:8080",
+            ErrorKind::AlphaNumeric
+        )))
+    );
+    assert_eq!(
+        ip("192.168.0.1444:8080"),
+        Err(NomErr::Error(Error::new(
+            "1444:8080",
+            ErrorKind::AlphaNumeric
+        )))
+    );
+    assert_eq!(
+        ip("192.168.0:8080"),
+        Err(NomErr::Error(Error::new("0:8080", ErrorKind::AlphaNumeric)))
+    );
 }
